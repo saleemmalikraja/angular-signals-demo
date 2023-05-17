5130b6f6467a647b7acc095c1f952d30aea75281
saleemmalikraja <saleemmalikraja@gmail.com>
2023-05-17 15:48:15 +0530

    3 files modified

--- src/demo1-why-signals/demo1-why-signals.component.ts
+++ src/demo1-why-signals/demo1-why-signals.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-demo1-why-signals',
   templateUrl: './demo1-why-signals.component.html',
-  styleUrls: ['./demo1-why-signals.component.css']
+  styleUrls: ['./demo1-why-signals.component.css'],
+  standalone: true,
+  imports: [MatCardModule],
 })
 export class Demo1WhySignalsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

60faff84390923611e0a814d948cc9ec3bf3f48f
saleemmalikraja <saleemmalikraja@gmail.com>
2023-05-20 20:41:03 +0530

    computed effect setup

---
+++ src/demo5-computed-effect/demo5-computed-effect.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo5-computed-effect',
+  templateUrl: './demo5-computed-effect.component.html',
+  styleUrls: ['./demo5-computed-effect.component.css'],
+})
+export class Demo5ComputedEffectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

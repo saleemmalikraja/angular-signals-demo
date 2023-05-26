0821e80463f343d1b2f3e0058a19d0c00173809f
saleemmalikraja <saleemmalikraja@gmail.com>
2023-05-26 11:43:19 +0530

    Added - welcome - discover angular signals

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [MatCardModule, CommonModule, MatDividerModule],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
